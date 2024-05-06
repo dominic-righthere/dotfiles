31dc97d3272eb7e35326603c29bfb8f3904276f3
dominic-righthere <64965775+dominic-righthere@users.noreply.github.com>
2024-05-06 16:23:51 +0900

    check directory

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,6 +1,6 @@
 echo "setup tmux..."
 
-if [ -f $HOME/.tmux/plugins/tpm ]; then
+if [ -d $HOME/.tmux/plugins/tpm ]; then
     echo "tpm exists"
 else
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
